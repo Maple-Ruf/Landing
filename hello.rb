c9c910f230d15ccc90262eaac8645b8c40c0aae0
Maple-Ruf <arnaud.delavison@hotmail.fr>
2017-10-12 19:30:08 +0200

    update

---
+++ hello.rb
@@ -0,0 +1,4 @@
+def hello
+	puts "Hello sir, very nice to meet you."
+end
+hello
---
+++ nice.rb
@@ -0,0 +1,25 @@
+#The price is right: the game !
+
+
+#Computer chooses a randome number between 1 and 50
+the_right_price = rand(1..50)
+
+until user_guess == 0
+	
+end
+#Ask a number to the user
+
+user_guess = gets.chomp.to_i
+#f user has the right price
+if the right price == user_guess puts "winner!"
+
+else the right price < user_guess puts "it's less"
+
+else puts "it's more!"
+
+end
+#He wins
+
+#Else, computer tips the user:
+
+#"+" or "-"
